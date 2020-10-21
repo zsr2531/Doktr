eb5a4d8ffccaa4f917a7f6a6fd2acc52a5ff13c4
zsr2531 <warhaamer1337@gmail.com>
2020-10-21 19:27:50 +0200

    Create project files

---
+++ src/Doctor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Doctor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
