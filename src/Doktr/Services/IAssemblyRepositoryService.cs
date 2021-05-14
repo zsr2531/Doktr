968d71f471dee2f9bd7efec8e62615a0a1811cac
zsr2531 <warhaamer1337@gmail.com>
2021-05-14 13:10:35 +0200

    Create assembly repository

---
+++ src/Doktr/Services/IAssemblyRepositoryService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using AsmResolver.DotNet;
+
+namespace Doktr.Services
+{
+    public interface IAssemblyRepositoryService
+    {
+        IReadOnlyList<AssemblyDefinition> LoadedAssemblies
+        {
+            get;
+        }
+
+        bool LoadAssembly(string path);
+    }
+}
