993b627989aea99daa80eb43d12c82f8e90cce80
zsr2531 <warhaamer1337@gmail.com>
2021-05-14 13:15:02 +0200

    Add OwnerCollection

---
+++ src/Doktr/Dependencies/OwnerCollection.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.ObjectModel;
+
+namespace Doktr.Dependencies
+{
+    public class OwnerCollection : Collection<DependencyNode>
+    {
+        private static void AssertNoParent(DependencyNode node)
+        {
+            if (node.Parent is not null)
+                throw new InvalidOperationException("Node already has a parent.");
+        }
+        
+        private readonly DependencyNode _owner;
+
+        public OwnerCollection(DependencyNode owner)
+        {
+            _owner = owner;
+        }
+
+        protected override void ClearItems()
+        {
+            foreach (var child in this)
+                child.Parent = null;
+            
+            base.ClearItems();
+        }
+
+        protected override void InsertItem(int index, DependencyNode item)
+        {
+            AssertNoParent(item);
+            item.Parent = _owner;
+            base.InsertItem(index, item);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            this[index].Parent = null;
+            base.RemoveItem(index);
+        }
+
+        protected override void SetItem(int index, DependencyNode item)
+        {
+            AssertNoParent(item);
+            item.Parent = _owner;
+            base.SetItem(index, item);
+        }
+    }
+}
