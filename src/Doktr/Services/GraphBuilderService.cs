8728e637db8f5db0435c2e002b16c8e3b5e27d98
zsr2531 <warhaamer1337@gmail.com>
2021-05-17 11:22:19 +0200

    Collect all reachable members in project in dependency graph

--- src/Doktr/Services/GraphBuilderService.cs
+++ src/Doktr/Services/GraphBuilderService.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using AsmResolver.DotNet;
 using Doktr.Dependencies;
 using Serilog;
@@ -8,11 +10,13 @@ namespace Doktr.Services
     public class GraphBuilderService : IGraphBuilderService
     {
         private readonly IAssemblyRepositoryService _repository;
+        private readonly IMetadataResolutionService _resolution;
         private readonly ILogger _logger;
 
-        public GraphBuilderService(IAssemblyRepositoryService repository, ILogger logger)
+        public GraphBuilderService(IAssemblyRepositoryService repository, IMetadataResolutionService resolution, ILogger logger)
         {
             _repository = repository;
+            _resolution = resolution;
             _logger = logger;
         }
 
@@ -20,16 +24,132 @@ namespace Doktr.Services
         {
             var mapping = ImmutableDictionary.CreateBuilder<IFullNameProvider, DependencyNode>();
             var roots = ImmutableArray.CreateBuilder<DependencyNode>();
-            
+            var topLevelTypes = new List<TypeDefinition>();
+            var context = new GraphBuilderContext(mapping);
+
             foreach (var assembly in _repository.LoadedAssemblies)
             {
                 _logger.Verbose("Processing '{Assembly}'", assembly.FullName);
-                var node = new DependencyNode(assembly);
-                roots.Add(node);
-                mapping.Add(assembly, node);
+                roots.Add(context.GetOrCreateNode(assembly));
+
+                topLevelTypes.AddRange(assembly.ManifestModule.TopLevelTypes);
             }
 
+            foreach (var type in topLevelTypes)
+                VisitType(type, context);
+
             return new DependencyGraph(mapping.ToImmutable(), roots.ToImmutable());
+        }
+
+        private void VisitType(TypeDefinition type, GraphBuilderContext context)
+        {
+            if (context.NodeExists(type))
+                return;
+            
+            _logger.Verbose("Visiting type {Type}...", type.FullName);
+            var typeNode = context.GetOrCreateNode(type, GetParent());
+            
+            VisitTypeMembers(type, context, typeNode);
+            VisitTypeAncestors(type, context);
+
+            DependencyNode GetParent()
+            {
+                if (type.DeclaringType is not { } decl)
+                    return context.GetOrCreateNode(type.Module.Assembly);
+                if (context.NodeExists(decl))
+                    return context.GetOrCreateNode(decl);
+                    
+                VisitType(decl, context);
+                return context.GetOrCreateNode(decl);
+            }
+        }
+
+        private void VisitTypeMembers(TypeDefinition type, GraphBuilderContext context, DependencyNode typeNode)
+        {
+            foreach (var @event in type.Events)
+                VisitEvent(@event, context, typeNode);
+
+            foreach (var field in type.Fields)
+                VisitField(field, context, typeNode);
+            
+            foreach (var property in type.Properties)
+                VisitProperty(property, context, typeNode);
+
+            foreach (var method in type.Methods.Where(m => !context.NodeExists(m)))
+                VisitMethod(method, context, typeNode);
+            
+            foreach (var nestedType in type.NestedTypes)
+                VisitType(nestedType, context);
+        }
+
+        private void VisitTypeAncestors(TypeDefinition type, GraphBuilderContext context)
+        {
+            if (type.BaseType is { } baseType && _resolution.ResolveType(baseType) is { } resolved)
+                VisitType(resolved, context);
+            
+            foreach (var impl in type.Interfaces)
+            {
+                var @interface = impl.Interface;
+                resolved = _resolution.ResolveType(@interface);
+                if (resolved is null)
+                    continue;
+                
+                VisitType(resolved, context);
+            }
+        }
+
+        private void VisitEvent(EventDefinition @event, GraphBuilderContext context, DependencyNode parent)
+        {
+            _logger.Verbose("Visiting event {Event}...", @event.FullName);
+            var node = context.GetOrCreateNode(@event, parent);
+
+            if (@event.AddMethod is { } add)
+            {
+                _logger.Verbose("Visiting add method {Method} of event {Event}...", add.FullName, @event.FullName);
+                context.GetOrCreateNode(add, node);
+            }
+
+            if (@event.RemoveMethod is { } remove)
+            {
+                _logger.Verbose("Visiting remove method {Method} of event {Event}...", remove.FullName, @event.FullName);
+                context.GetOrCreateNode(remove, node);
+            }
+
+            if (@event.FireMethod is { } fire)
+            {
+                _logger.Verbose("Visiting fire method {Method} of event {Event}...", fire.FullName, @event.FullName);
+                context.GetOrCreateNode(fire, node);
+            }
+        }
+
+        private void VisitField(FieldDefinition field, GraphBuilderContext context, DependencyNode parent)
+        {
+            _logger.Verbose("Visiting field {Field}...", field.FullName);
+            context.GetOrCreateNode(field, parent);
+        }
+
+        private void VisitProperty(PropertyDefinition property, GraphBuilderContext context, DependencyNode parent)
+        {
+            _logger.Verbose("Visiting property {Property}...", property.FullName);
+            var node = context.GetOrCreateNode(property, parent);
+
+            if (property.GetMethod is { } get)
+            {
+                _logger.Verbose("Visiting getter method {Method} of property {Property}...", get.FullName, property.FullName);
+                context.GetOrCreateNode(get, node);
+            }
+
+            if (property.SetMethod is { } set)
+            {
+                _logger.Verbose("Visiting setter method {Method} of property {Property}...", set.FullName, property.FullName);
+                context.GetOrCreateNode(set, node);
+            }
+        }
+
+        private void VisitMethod(MethodDefinition method, GraphBuilderContext context, DependencyNode parent)
+        {
+            _logger.Verbose("Visiting method {Method}...", method.FullName);
+            context.GetOrCreateNode(method, parent);
         }
     }
 }
