81764cfcccfc7f244532b59cc87329c444f3ddfd
zsr2531 <warhaamer1337@gmail.com>
2021-05-17 11:21:03 +0200

    Create metadata resolution service

---
+++ src/Doktr/Services/IMetadataResolutionService.cs
@@ -0,0 +1,13 @@
+using AsmResolver.DotNet;
+
+namespace Doktr.Services
+{
+    public interface IMetadataResolutionService
+    {
+        TypeDefinition? ResolveType(ITypeDefOrRef type);
+
+        MethodDefinition? ResolveMethod(IMethodDefOrRef method);
+
+        IMetadataMember? ResolveMember(IMemberDescriptor descriptor);
+    }
+}
---
+++ src/Doktr/Services/MetadataResolutionService.cs
@@ -0,0 +1,42 @@
+using AsmResolver.DotNet;
+using Serilog;
+
+namespace Doktr.Services
+{
+    public class MetadataResolutionService : IMetadataResolutionService
+    {
+        private readonly ILogger _logger;
+
+        public MetadataResolutionService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public TypeDefinition? ResolveType(ITypeDefOrRef type)
+        {
+            var resolved = type.Resolve();
+            if (resolved is null)
+                _logger.Warning("Failed to resolve type reference {Type}.", type);
+
+            return resolved;
+        }
+
+        public MethodDefinition? ResolveMethod(IMethodDefOrRef method)
+        {
+            var resolved = method.Resolve();
+            if (resolved is null)
+                _logger.Warning("Failed to resolve method reference {Method}.", method);
+
+            return resolved;
+        }
+
+        public IMetadataMember? ResolveMember(IMemberDescriptor descriptor)
+        {
+            var resolved = descriptor.Resolve();
+            if (resolved is null)
+                _logger.Warning("Failed to resolve member reference {Member}.", descriptor);
+
+            return resolved;
+        }
+    }
+}
