b11cecee7cf6709b219f894363f5e12a03f178b2
zsr2531 <warhaamer1337@gmail.com>
2022-02-07 22:20:00 +0100

    Add link doc segment

---
+++ src/Doktr.Models/Segments/LinkDocumentationSegment.cs
@@ -0,0 +1,16 @@
+﻿namespace Doktr.Models.Segments;
+
+public class LinkDocumentationSegment : IDocumentationSegment
+{
+    public LinkDocumentationSegment(string url)
+    {
+        Url = url;
+    }
+
+    public string Url
+    {
+        get;
+    }
+    
+    public void AcceptVisitor(IDocumentationSegmentVisitor visitor) => visitor.Visit(this);
+}
