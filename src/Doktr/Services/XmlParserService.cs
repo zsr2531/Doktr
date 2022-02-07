1ddbd2673f282e07c8439fca98fc31a4bebd28e6
zsr2531 <warhaamer1337@gmail.com>
2022-02-07 22:27:20 +0100

    Implement IXmlParserService

---
+++ src/Doktr/Services/XmlParserService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using Doktr.Services.XmlDocParser;
+using Serilog;
+
+namespace Doktr.Services;
+
+public class XmlParserService : IXmlParserService
+{
+    private readonly ILogger _logger;
+
+    public XmlParserService(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public ImmutableDictionary<string, XmlDocEntry> ParseXmlFile(string raw)
+    {
+        var tokens = new Lexer(raw).CollectTokens();
+        var parsed = new Parser(tokens).Parse(out var diagnostics);
+        foreach (string diag in diagnostics)
+            _logger.Error("Error while parsing xmldoc: '{}'", diag);
+
+        return parsed;
+    }
+}
