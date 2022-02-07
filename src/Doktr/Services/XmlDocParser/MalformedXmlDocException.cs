b7b36b1aa9ce312ccb9bda2650cd9c85662dc51a
zsr2531 <warhaamer1337@gmail.com>
2022-02-07 22:26:35 +0100

    Add custom xml exceptioon type

---
+++ src/Doktr/Services/XmlDocParser/MalformedXmlDocException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Doktr.Services.XmlDocParser;
+
+public class MalformedXmlDocException : Exception
+{
+    public MalformedXmlDocException(string message)
+        : base(message)
+    {
+    }
+
+    public MalformedXmlDocException(string got, params string[] expected)
+        : base($"Expected one of: {{ '{string.Join("', '", expected)}' }}, but instead got: '{got}'")
+    {
+    }
+}
