f3f92f0c55c893bb49edd190154d8f6ffc25338d
zsr2531 <warhaamer1337@gmail.com>
2022-02-07 22:30:22 +0100

    Add a test for type sig decompilation

---
+++ test/Doktr.SemanticDocumentationValidatorTests/TestCases/Nullables.cs
@@ -0,0 +1,10 @@
+﻿namespace Doktr.SemanticDocumentationValidatorTests.TestCases;
+
+#nullable enable
+
+public struct Struct<T>
+{
+}
+
+public delegate object Delegate(object? a, (dynamic, (int, int), string? Heyy) b, int c);
+// public delegate object Delegate(Struct<int>? p);
