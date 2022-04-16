b3aab2016e4ae00ca07cc7a88cadd30d5e20b45a
zsr2531 <warhaamer1337@gmail.com>
2022-04-16 15:55:22 +0200

    Add collection documentation fragments

---
+++ src/Doktr.Core/Models/Collections/DocumentationFragmentCollection.cs
@@ -0,0 +1,8 @@
+using System.Collections.ObjectModel;
+using Doktr.Core.Models.Fragments;
+
+namespace Doktr.Core.Models.Collections;
+
+public class DocumentationFragmentCollection : Collection<DocumentationFragment>
+{
+}
