935152b16ffd51ee541475a4d85d650b79d551e9
zsr2531 <warhaamer1337@gmail.com>
2022-04-19 00:47:35 +0200

    Finalize method documentation model

--- src/Doktr.Core/Models/MethodDocumentation.cs
+++ src/Doktr.Core/Models/MethodDocumentation.cs
@@ -2,16 +2,25 @@ using Doktr.Core.Models.Collections;
 
 namespace Doktr.Core.Models;
 
-public class MethodDocumentation : MemberDocumentation, IHasVirtual, IHasTypeParameters, IHasParameters, IHasExceptions
+public class MethodDocumentation : MemberDocumentation,
+    IHasStatic,
+    IHasReadOnly,
+    IHasVirtual,
+    IHasTypeParameters,
+    IHasParameters,
+    IHasExceptions
 {
     public MethodDocumentation(string name, MemberVisibility visibility)
         : base(name, visibility)
     {
     }
 
+    public bool IsStatic { get; set; } = false;
+    public bool IsReadOnly { get; set; } = false;
     public bool IsVirtual { get; set; } = false;
     public bool IsOverride { get; set; } = false;
     public bool IsSealed { get; set; } = false;
+    public bool IsExtension { get; set; } = false;
     public TypeParameterSegmentCollection TypeParameters { get; set; } = new();
     public ParameterSegmentCollection Parameters { get; set; } = new();
     public DocumentationFragmentCollection Returns { get; set; } = new();
