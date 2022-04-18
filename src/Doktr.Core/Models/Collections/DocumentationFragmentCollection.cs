0579ab611e64dece7d2b5092fb1b2709aab64813
zsr2531 <warhaamer1337@gmail.com>
2022-04-18 16:58:26 +0200

    Add convenience visitor helper in DocumentationFragmentCollection

--- src/Doktr.Core/Models/Collections/DocumentationFragmentCollection.cs
+++ src/Doktr.Core/Models/Collections/DocumentationFragmentCollection.cs
@@ -3,6 +3,11 @@ using Doktr.Core.Models.Fragments;
 
 namespace Doktr.Core.Models.Collections;
 
-public class DocumentationFragmentCollection : Collection<DocumentationFragment>
+public class DocumentationFragmentCollection : Collection<IDocumentationFragment>
 {
+    public void AcceptVisitor(IDocumentationFragmentVisitor visitor)
+    {
+        foreach (var fragment in this)
+            fragment.AcceptVisitor(visitor);
+    }
 }
