67e6282e6e1c97fd5a1a253a3120d1d5409c5da2
zsr2531 <warhaamer1337@gmail.com>
2022-04-18 17:53:44 +0200

    Add all visibility modifiers

--- src/Doktr.Core/Models/MemberVisibility.cs
+++ src/Doktr.Core/Models/MemberVisibility.cs
@@ -2,6 +2,20 @@ namespace Doktr.Core.Models;
 
 public enum MemberVisibility
 {
-    Public,
-    Protected
+    Private,
+    ProtectedAndAssembly,
+    Assembly,
+    Protected,
+    ProtectedOrAssembly,
+    Public
+}
+
+public static class MemberVisibilityExtensions
+{
+    public static bool IsVisibleOutsideAssembly(this MemberVisibility memberVisibility)
+    {
+        return memberVisibility is MemberVisibility.Public
+            or MemberVisibility.Protected
+            or MemberVisibility.ProtectedOrAssembly;
+    }
 }
