0eb0fb9491de4a032ff040c4b0d2d61c7faed712
zsr2531 <warhaamer1337@gmail.com>
2022-04-19 00:47:24 +0200

    Improve field documentation model capabilities

--- src/Doktr.Core/Models/FieldDocumentation.cs
+++ src/Doktr.Core/Models/FieldDocumentation.cs
@@ -3,7 +3,7 @@ using Doktr.Core.Models.Signatures;
 
 namespace Doktr.Core.Models;
 
-public class FieldDocumentation : MemberDocumentation, IHasValue
+public class FieldDocumentation : MemberDocumentation, IHasStatic, IHasReadOnly, IHasValue
 {
     public FieldDocumentation(string name, MemberVisibility visibility, TypeSignature type)
         : base(name, visibility)
@@ -12,5 +12,9 @@ public class FieldDocumentation : MemberDocumentation, IHasValue
     }
     
     public TypeSignature Type { get; set; }
+    public bool IsStatic { get; set; } = false;
+    public bool IsVolatile { get; set; } = false;
+    public bool IsConstant { get; set; } = false;
+    public bool IsReadOnly { get; set; } = false;
     public DocumentationFragmentCollection Value { get; set; } = new();
 }
