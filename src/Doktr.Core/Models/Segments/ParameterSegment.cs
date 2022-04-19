c8dff7c609e37e90bb0f9b602345868c1895f6e2
zsr2531 <warhaamer1337@gmail.com>
2022-04-19 11:46:49 +0200

    Add parameter modifiers

--- src/Doktr.Core/Models/Segments/ParameterSegment.cs
+++ src/Doktr.Core/Models/Segments/ParameterSegment.cs
@@ -1,15 +1,36 @@
 using Doktr.Core.Models.Collections;
+using Doktr.Core.Models.Signatures;
 
 namespace Doktr.Core.Models.Segments;
 
-// TODO: Modifiers! (in, out, ref)
+[Flags]
+public enum ParameterModifierFlags
+{
+    None = 0,
+    In = 1,
+    Out = 2,
+    Ref = 4,
+    Optional = 8,
+    Params = 16
+}
+
 public class ParameterSegment
 {
-    public ParameterSegment(string name)
+    public ParameterSegment(TypeSignature type, string name)
     {
+        Type = type;
         Name = name;
     }
 
+    public TypeSignature Type { get; set; }
     public string Name { get; set; }
+    public ParameterModifierFlags Modifiers { get; set; } = ParameterModifierFlags.None;
+    public object? DefaultValue { get; set; } = null;
     public DocumentationFragmentCollection Documentation { get; set; } = new();
+    public bool IsIn => (Modifiers & ParameterModifierFlags.In) != 0;
+    public bool IsOut => (Modifiers & ParameterModifierFlags.Out) != 0;
+    public bool IsRef => (Modifiers & ParameterModifierFlags.Ref) != 0;
+    public bool IsOptional => (Modifiers & ParameterModifierFlags.Optional) != 0;
+    public bool IsParams => (Modifiers & ParameterModifierFlags.Params) != 0;
+    public bool HasDefaultValue => IsOptional;
 }
