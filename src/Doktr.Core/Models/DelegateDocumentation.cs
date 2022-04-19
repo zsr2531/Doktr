15817e18679e0fd80f5792b2f04dd9ce1b5ac304
zsr2531 <warhaamer1337@gmail.com>
2022-04-19 21:52:03 +0200

    Add delegate documentation model

---
+++ src/Doktr.Core/Models/DelegateDocumentation.cs
@@ -0,0 +1,34 @@
+using Doktr.Core.Models.Collections;
+using Doktr.Core.Models.Signatures;
+
+namespace Doktr.Core.Models;
+
+public class DelegateDocumentation : TypeDocumentation, IHasParameters, IHasReturns
+{
+    public DelegateDocumentation(string name, MemberVisibility visibility, TypeSignature returnType)
+        : base(name, visibility)
+    {
+        ReturnType = returnType;
+    }
+
+    public ParameterSegmentCollection Parameters { get; set; } = new();
+    public TypeSignature ReturnType { get; set; }
+    public DocumentationFragmentCollection Returns { get; set; } = new();
+
+    public override DelegateDocumentation Clone()
+    {
+        var clone = new DelegateDocumentation(Name, Visibility, ReturnType.Clone());
+        CopyDocumentationTo(clone);
+
+        return clone;
+    }
+
+    protected override void CopyDocumentationTo(MemberDocumentation other)
+    {
+        if (other is not DelegateDocumentation otherDelegate)
+            throw new InvalidOperationException("Cannot copy documentation to a non-delegate member.");
+        
+        otherDelegate.Parameters = Parameters.Clone();
+        otherDelegate.Returns = Returns.Clone();
+    }
+}
