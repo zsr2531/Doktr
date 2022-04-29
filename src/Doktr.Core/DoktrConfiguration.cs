01ac1a4aac445e856eb4e116d5b32699c72cd332
zsr2531 <warhaamer1337@gmail.com>
2022-04-29 14:18:04 +0200

    Make NRT enabled by default

--- src/Doktr.Core/DoktrConfiguration.cs
+++ src/Doktr.Core/DoktrConfiguration.cs
@@ -2,7 +2,7 @@ namespace Doktr.Core;
 
 public class DoktrConfiguration
 {
-    public bool EnableNrt { get; set; }
+    public bool EnableNrt { get; set; } = true;
     public string[] XmlDocProbePaths { get; set; } = Array.Empty<string>();
     public string[] XrefUrls { get; set; } = Array.Empty<string>();
 }
