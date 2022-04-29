d862de208d14724c87e176397b4be805f8c752e2
zsr2531 <warhaamer1337@gmail.com>
2022-04-29 14:21:09 +0200

    Add unit tests for type signature decompiler

---
+++ test/Doktr.Decompiler.Tests/Signatures/GenericInstanceTypeSignatureTests.cs
@@ -0,0 +1,97 @@
+using Doktr.Core.Models;
+using Doktr.Core.Models.Signatures;
+using Doktr.Decompiler.Signatures;
+using Xunit;
+
+namespace Doktr.Decompiler.Tests.Signatures;
+
+public class GenericInstanceTypeSignatureTests
+{
+    [Fact]
+    public void CustomType_With_GenericParameter_Normal()
+    {
+        var genericTypeSignature = new VanillaTypeSignature("Test", new CodeReference("T:My.Test"));
+        var genericParameter = new GenericParameterTypeSignature("T");
+        var signature = new GenericInstanceTypeSignature(genericTypeSignature);
+        signature.TypeArguments.Add(genericParameter);
+        var decompiler = new TypeSignatureDecompilationStrategy();
+        signature.AcceptVisitor(decompiler);
+
+        string decompiled = decompiler.ToString();
+        Assert.Equal("Test<T>", decompiled);
+    }
+
+    [Fact]
+    public void CustomType_With_Multiple_GenericParameters_Normal()
+    {
+        var genericTypeSignature = new VanillaTypeSignature("Test", new CodeReference("T:My.Test"));
+        var genericParameter = new GenericParameterTypeSignature("T");
+        var genericParameter2 = new GenericParameterTypeSignature("U");
+        var genericParameter3 = new GenericParameterTypeSignature("V");
+        var signature = new GenericInstanceTypeSignature(genericTypeSignature);
+        signature.TypeArguments.Add(genericParameter);
+        signature.TypeArguments.Add(genericParameter2);
+        signature.TypeArguments.Add(genericParameter3);
+        var decompiler = new TypeSignatureDecompilationStrategy();
+        signature.AcceptVisitor(decompiler);
+
+        string decompiled = decompiler.ToString();
+        Assert.Equal("Test<T, U, V>", decompiled);
+    }
+
+    [Fact]
+    public void CustomType_With_Nullable_GenericParameter_Normal()
+    {
+        var genericTypeSignature = new VanillaTypeSignature("Test", new CodeReference("T:My.Test"));
+        var genericParameter = new GenericParameterTypeSignature("T")
+        {
+            Nullability = NullabilityKind.Nullable
+        };
+        var signature = new GenericInstanceTypeSignature(genericTypeSignature);
+        signature.TypeArguments.Add(genericParameter);
+        var decompiler = new TypeSignatureDecompilationStrategy();
+        signature.AcceptVisitor(decompiler);
+
+        string decompiled = decompiler.ToString();
+        Assert.Equal("Test<T>", decompiled);
+    }
+
+    [Fact]
+    public void CustomType_With_Nullable_GenericParameter_Nullable()
+    {
+        var genericTypeSignature = new VanillaTypeSignature("Test", new CodeReference("T:My.Test"));
+        var genericParameter = new GenericParameterTypeSignature("T")
+        {
+            Nullability = NullabilityKind.Nullable
+        };
+        var signature = new GenericInstanceTypeSignature(genericTypeSignature);
+        signature.TypeArguments.Add(genericParameter);
+        var decompiler = new NullableTypeSignatureDecompilationStrategy();
+        signature.AcceptVisitor(decompiler);
+
+        string decompiled = decompiler.ToString();
+        Assert.Equal("Test<T?>", decompiled);
+    }
+
+    [Fact]
+    public void Custom_Type_With_Normal_And_Nullable_GenericParameters_Nullable()
+    {
+        var genericTypeSignature = new VanillaTypeSignature("Test", new CodeReference("T:My.Test"));
+        var genericParameter = new GenericParameterTypeSignature("T")
+        {
+            Nullability = NullabilityKind.Nullable
+        };
+        var genericParameter2 = new GenericParameterTypeSignature("U")
+        {
+            Nullability = NullabilityKind.Nullable
+        };
+        var signature = new GenericInstanceTypeSignature(genericTypeSignature);
+        signature.TypeArguments.Add(genericParameter);
+        signature.TypeArguments.Add(genericParameter2);
+        var decompiler = new NullableTypeSignatureDecompilationStrategy();
+        signature.AcceptVisitor(decompiler);
+
+        string decompiled = decompiler.ToString();
+        Assert.Equal("Test<T?, U?>", decompiled);
+    }
+}
