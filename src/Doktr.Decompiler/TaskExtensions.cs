72ef6d62ce7bedae8c2578f9ead357789233915c
zsr2531 <warhaamer1337@gmail.com>
2022-04-29 14:18:23 +0200

    Make helper extension method internal

--- src/Doktr.Decompiler/TaskExtensions.cs
+++ src/Doktr.Decompiler/TaskExtensions.cs
@@ -1,6 +1,6 @@
 namespace Doktr.Decompiler;
 
-public static class TaskExtensions
+internal static class TaskExtensions
 {
-    public static T WaitForResult<T>(this Task<T> task) => task.GetAwaiter().GetResult();
+    internal static T WaitForResult<T>(this Task<T> task) => task.GetAwaiter().GetResult();
 }
