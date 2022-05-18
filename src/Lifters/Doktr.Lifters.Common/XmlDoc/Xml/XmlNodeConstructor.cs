f51553268424322d7ed1d52da0545ee8f412d663
zsr2531 <warhaamer1337@gmail.com>
2022-05-18 15:48:24 +0200

    Create ANTLR parse tree -> node model visitor

---
+++ src/Lifters/Doktr.Lifters.Common/XmlDoc/Xml/XmlNodeConstructor.cs
@@ -0,0 +1,80 @@
+using Antlr4.Runtime;
+
+namespace Doktr.Lifters.Common.XmlDoc.Xml;
+
+public class XmlNodeConstructor : XmlBaseVisitor<XmlNode>
+{
+    public static readonly XmlNodeConstructor Instance = new();
+
+    private XmlNodeConstructor()
+    {
+    }
+
+    public override XmlNode VisitElement(Xml.ElementContext context)
+    {
+        var span = CreateTextSpan(context);
+        string name = context.IDENTIFIER().GetText();
+        var node = new XmlElementNode(span, name);
+        var rawAttributes = context.attribute();
+        AddAttributes(node, rawAttributes);
+
+        return node;
+    }
+
+    public override XmlNode VisitEndElement(Xml.EndElementContext context)
+    {
+        var span = CreateTextSpan(context);
+        string name = context.IDENTIFIER().GetText();
+
+        return new XmlEndElementNode(span, name);
+    }
+
+    public override XmlNode VisitEmptyElement(Xml.EmptyElementContext context)
+    {
+        var span = CreateTextSpan(context);
+        string name = context.IDENTIFIER().GetText();
+        var node = new XmlEmptyElementNode(span, name);
+        var rawAttributes = context.attribute();
+        AddAttributes(node, rawAttributes);
+
+        return node;
+    }
+
+    public override XmlNode VisitTextElement(Xml.TextElementContext context)
+    {
+        var span = CreateTextSpan(context);
+        string text = context.TEXT().GetText();
+
+        return new XmlTextNode(span, text);
+    }
+
+    private static TextSpan CreateTextSpan(ParserRuleContext context)
+    {
+        var start = context.Start;
+        var stop = context.Stop;
+
+        return new TextSpan(start.Line, start.Column, stop.Line, stop.Column);
+    }
+
+    private static void AddAttributes(IHasAttributes node, Xml.AttributeContext[] attributes)
+    {
+        foreach (var attribute in attributes)
+        {
+            string key = attribute.IDENTIFIER().GetText();
+            string value = attribute.@string().Accept(StringVisitor.Instance);
+            node.Attributes[key] = value;
+        }
+    }
+
+    private class StringVisitor : XmlBaseVisitor<string>
+    {
+        // ReSharper disable once MemberHidesStaticFromOuterClass
+        internal static readonly StringVisitor Instance = new();
+
+        private StringVisitor()
+        {
+        }
+
+        public override string VisitString(Xml.StringContext context) => context.GetText()[1..^1];
+    }
+}
