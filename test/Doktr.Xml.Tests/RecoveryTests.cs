b67061ce3be7bb167012521313c154c064fa8dbd
zsr2531 <warhaamer1337@gmail.com>
2022-05-19 22:37:08 +0200

    Add extra unit test for parser recovery

--- test/Doktr.Xml.Tests/RecoveryTests.cs
+++ test/Doktr.Xml.Tests/RecoveryTests.cs
@@ -12,8 +12,23 @@ public class RecoveryTests
         var diagnostics = parser.Diagnostics;
 
         Assert.NotEmpty(diagnostics);
-        Assert.False(diagnostics.HasFatal);
         var node = Assert.IsType<XmlElementNode>(Assert.Single(result));
         Assert.Equal("hello", node.Name);
+    }
+
+    [Fact]
+    public void Incomplete_Attribute_With_Newline()
+    {
+        var parser = new XmlParser("<hello version='1.0>\n<hello>");
+        var result = parser.ParseXmlNodes();
+        var diagnostics = parser.Diagnostics;
+
+        Assert.NotEmpty(diagnostics);
+        Assert.Equal(2, result.Count);
+        var first = Assert.IsType<XmlTextNode>(result[0]);
+        var second = Assert.IsType<XmlElementNode>(result[1]);
+
+        Assert.Equal("\n", first.Text);
+        Assert.Equal("hello", second.Name);
     }
 }
