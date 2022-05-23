18c8a61b4875c6690db2ed320b617844dd605eb0
zsr2531 <warhaamer1337@gmail.com>
2022-05-23 19:07:08 +0200

    Create contract for xmldoc parser

---
+++ src/Doktr.Xml/XmlDoc/IXmlDocProcessor.cs
@@ -0,0 +1,22 @@
+using Doktr.Core.Models.Fragments;
+
+namespace Doktr.Xml.XmlDoc;
+
+public interface IXmlDocProcessor
+{
+    XmlNode? Lookahead { get; }
+
+    DocumentationFragment NextFragment();
+
+    XmlElementNode ExpectElement(params string[] names);
+
+    XmlEndElementNode ExpectEndElement(string name);
+
+    XmlEmptyElementNode ExpectEmptyElement(params string[] names);
+
+    XmlTextNode ExpectText();
+
+    XmlNode ExpectElementOrEmptyElement(params string[] names);
+
+    XmlNode Consume();
+}
