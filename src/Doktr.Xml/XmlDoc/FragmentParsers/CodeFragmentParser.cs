0f8d791caa6ec6703f0b92ef0300e25161fe970b
zsr2531 <warhaamer1337@gmail.com>
2022-05-28 09:14:18 +0200

    Allow non-text content inside code fragments

--- src/Doktr.Xml/XmlDoc/FragmentParsers/CodeFragmentParser.cs
+++ src/Doktr.Xml/XmlDoc/FragmentParsers/CodeFragmentParser.cs
@@ -7,16 +7,18 @@ public class CodeFragmentParser : IFragmentParser
 {
     public string[] SupportedTags { get; } = { "code" };
 
+    // TODO: Maybe warn for non-text content?
     public DocumentationFragment ParseFragment(IXmlDocProcessor processor)
     {
         var fragment = new CodeFragment();
         var start = processor.ExpectElement(SupportedTags);
-        var sb = new StringBuilder();
+        // while (processor.Lookahead.IsNotEndElementOrNull())
+        // {
+        //     var text = processor.ExpectText();
+        //     sb.Append(text.Text);
+        // }
         while (processor.Lookahead.IsNotEndElementOrNull())
-        {
-            var text = processor.ExpectText();
-            sb.Append(text.Text);
-        }
+            fragment.Content.Add(processor.NextFragment());
 
         processor.ExpectEndElement(start.Name);
         return fragment;
