b6f98bf512fe72dc7b496cb5de7a0e0afb9a48a3
zsr2531 <warhaamer1337@gmail.com>
2022-05-28 09:14:55 +0200

    Add unit tests for primitive fragments

---
+++ test/Doktr.Xml.Tests/XmlDoc/Fragments/ItalicFragmentTests.cs
@@ -0,0 +1,35 @@
+using Doktr.Core.Models.Fragments;
+using Doktr.Xml.XmlDoc.FragmentParsers;
+using FluentAssertions;
+using Xunit;
+
+namespace Doktr.Xml.Tests.XmlDoc.Fragments;
+
+public class ItalicFragmentTests : FragmentTests
+{
+    public ItalicFragmentTests()
+        : base(new ItalicFragmentParser())
+    {
+    }
+
+    [Fact]
+    public void Italic()
+    {
+        var entry = GetSummaryFor("<i>Hello</i>");
+
+        var italic = AssertSingleChildIsType<ItalicFragment>(entry);
+        var text = AssertSingleChildIsType<TextFragment>(italic.Children);
+        text.Text.Should().Be("Hello");
+    }
+
+    [Fact]
+    public void Nested_Italic()
+    {
+        var entry = GetSummaryFor("<i><i>Hello</i></i>");
+
+        var italic1 = AssertSingleChildIsType<ItalicFragment>(entry);
+        var italic2 = AssertSingleChildIsType<ItalicFragment>(italic1.Children);
+        var text = AssertSingleChildIsType<TextFragment>(italic2.Children);
+        text.Text.Should().Be("Hello");
+    }
+}
---
+++ test/Doktr.Xml.Tests/XmlDoc/Fragments/LineBreakFragmentTests.cs
@@ -0,0 +1,21 @@
+using Doktr.Core.Models.Fragments;
+using Doktr.Xml.XmlDoc.FragmentParsers;
+using Xunit;
+
+namespace Doktr.Xml.Tests.XmlDoc.Fragments;
+
+public class LineBreakFragmentTests : FragmentTests
+{
+    public LineBreakFragmentTests()
+        : base(new LineBreakFragmentParser())
+    {
+    }
+
+    [Fact]
+    public void LineBreak()
+    {
+        var entry = GetSummaryFor("<br/>");
+
+        AssertSingleChildIsType<LineBreakFragment>(entry);
+    }
+}
