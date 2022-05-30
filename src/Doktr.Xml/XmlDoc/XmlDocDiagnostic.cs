9a36a52a5188ceef56712f10cc225268fc6730f4
zsr2531 <warhaamer1337@gmail.com>
2022-05-30 18:06:47 +0200

    Remove nullability from TExtSpan in XmlDocDiagnostic model

--- src/Doktr.Xml/XmlDoc/XmlDocDiagnostic.cs
+++ src/Doktr.Xml/XmlDoc/XmlDocDiagnostic.cs
@@ -2,26 +2,15 @@ namespace Doktr.Xml.XmlDoc;
 
 public readonly struct XmlDocDiagnostic
 {
-    public static XmlDocDiagnostic MakeWarning(string message, Exception? exception = null) =>
-        new(XmlDocDiagnosticSeverity.Warning, message, exception);
-
     public static XmlDocDiagnostic MakeWarning(TextSpan span, string message, Exception? exception = null) =>
         new(XmlDocDiagnosticSeverity.Warning, span, message, exception);
-
-    public static XmlDocDiagnostic MakeError(string message, Exception? exception = null) =>
-        new(XmlDocDiagnosticSeverity.Error, message, exception);
 
     public static XmlDocDiagnostic MakeError(TextSpan span, string message, Exception? exception = null) =>
         new(XmlDocDiagnosticSeverity.Error, span, message, exception);
 
-    private XmlDocDiagnostic(XmlDocDiagnosticSeverity severity, string message, Exception? exception = null)
-        : this(severity, null, message, exception)
-    {
-    }
-
     private XmlDocDiagnostic(
         XmlDocDiagnosticSeverity severity,
-        TextSpan? span,
+        TextSpan span,
         string message,
         Exception? exception = null)
     {
@@ -32,9 +21,9 @@ public readonly struct XmlDocDiagnostic
     }
 
     public XmlDocDiagnosticSeverity Severity { get; }
-    public TextSpan? Span { get; }
+    public TextSpan Span { get; }
     public string Message { get; }
     public Exception? Exception { get; }
 
-    public override string ToString() => Span is null ? $"{Message}." : $"({Span}): {Message}.";
+    public override string ToString() => $"({Span}): {Message}.";
 }
