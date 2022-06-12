5a41746ef3d84f81a95dc3b0f93ae4f1012d4f23
zsr2531 <warhaamer1337@gmail.com>
2022-06-12 23:14:34 +0200

    Add base constructor call dependency graph analyzer

---
+++ src/Lifters/Doktr.Lifters.AsmResolver/DependencyGraph/Analyzers/BaseConstructorAnalyzer.cs
@@ -0,0 +1,70 @@
+using System.Diagnostics.CodeAnalysis;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.PE.DotNet.Cil;
+using Doktr.Lifters.Common.DependencyGraph;
+using Serilog;
+
+namespace Doktr.Lifters.AsmResolver.DependencyGraph.Analyzers;
+
+public class BaseConstructorAnalyzer : IDependencyGraphAnalyzer<IMemberDefinition>
+{
+    private readonly ILogger _logger;
+
+    public BaseConstructorAnalyzer(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void AnalyzeNode(DependencyNode<IMemberDefinition> node)
+    {
+        if (node.Value is not MethodDefinition method)
+            return;
+        if (!IsInstanceConstructor(method))
+            return;
+        if (method.CilMethodBody is not { } body)
+            return;
+
+        var candidate = AnalyzeBody(body);
+        if (candidate is null)
+        {
+            _logger.Warning("Failed to resolve the base constructor call of {Ctor}", method);
+            return;
+        }
+
+        _logger.Verbose("The base constructor call of {Ctor} is {BaseCtor}", method, candidate);
+        node.ParentGraph.AddDependency(method, candidate);
+    }
+
+    private static MethodDefinition? AnalyzeBody(CilMethodBody body)
+    {
+        foreach (var instruction in body.Instructions)
+        {
+            if (!IsCall(instruction, out var target))
+                continue;
+
+            var resolved = target.Resolve();
+            if (resolved is not null && IsInstanceConstructor(resolved))
+                return resolved;
+        }
+
+        return null;
+    }
+
+    private static bool IsInstanceConstructor(MethodDefinition method)
+    {
+        return method.IsConstructor && !method.IsStatic;
+    }
+
+    private static bool IsCall(CilInstruction instruction, [NotNullWhen(true)] out IMethodDefOrRef? target)
+    {
+        target = null;
+        if (instruction.OpCode.Code != CilCode.Call)
+            return false;
+        if (instruction.Operand is not IMethodDefOrRef method)
+            return false;
+
+        target = method;
+        return true;
+    }
+}
