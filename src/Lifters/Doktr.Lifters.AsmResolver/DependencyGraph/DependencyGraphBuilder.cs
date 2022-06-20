94fee49fbd62283966314900635246baa2a94114
zsr2531 <warhaamer1337@gmail.com>
2022-06-21 00:39:18 +0200

    Small name change so it looks better

--- src/Lifters/Doktr.Lifters.AsmResolver/DependencyGraph/DependencyGraphBuilder.cs
+++ src/Lifters/Doktr.Lifters.AsmResolver/DependencyGraph/DependencyGraphBuilder.cs
@@ -5,7 +5,7 @@ namespace Doktr.Lifters.AsmResolver.DependencyGraph;
 
 public class DependencyGraphBuilder : IDependencyGraphBuilder<IMemberDefinition>
 {
-    private readonly List<IDependencyGraphAnalyzer<IMemberDefinition>> _analyzers;
+    private readonly IEnumerable<IDependencyGraphAnalyzer<IMemberDefinition>> _analyzers;
     private readonly ModuleDefinition _module;
     private readonly DependencyGraph<IMemberDefinition> _depGraph = new();
 
@@ -13,27 +13,27 @@ public class DependencyGraphBuilder : IDependencyGraphBuilder<IMemberDefinition>
         IEnumerable<IDependencyGraphAnalyzer<IMemberDefinition>> analyzers,
         ModuleDefinition module)
     {
-        _analyzers = analyzers.ToList();
+        _analyzers = analyzers;
         _module = module;
     }
 
     public DependencyGraph<IMemberDefinition> BuildDependencyGraph()
     {
-        AddTypes();
+        InspectTypes();
         PerformAnalysis();
 
         return _depGraph;
     }
 
-    private void AddTypes()
+    private void InspectTypes()
     {
         foreach (var type in _module.TopLevelTypes)
-            AddType(type);
+            InspectType(type);
     }
 
-    private void AddType(TypeDefinition type)
+    private void InspectType(TypeDefinition type)
     {
-        _depGraph.AddNode(type);
+        AddMember(type);
         foreach (var ev in type.Events)
             AddMember(ev);
         foreach (var field in type.Fields)
@@ -43,7 +43,7 @@ public class DependencyGraphBuilder : IDependencyGraphBuilder<IMemberDefinition>
         foreach (var method in type.Methods)
             AddMember(method);
         foreach (var nestedType in type.NestedTypes)
-            AddType(nestedType);
+            InspectType(nestedType);
     }
 
     private void AddMember(IMemberDefinition member) => _depGraph.AddNode(member);
