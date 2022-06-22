703e1e34f5e3f730dd3ab36d0e89ed33ecf50a6d
zsr2531 <warhaamer1337@gmail.com>
2022-06-22 23:01:27 +0200

    Create a fixture for analyzers

---
+++ test/Lifters/Doktr.Lifters.AsmResolver.Tests/DependencyGraph/AnalyzerFixture.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using AsmResolver.DotNet;
+using Doktr.Lifters.AsmResolver.DependencyGraph;
+using Doktr.Lifters.Common.DependencyGraph;
+using FluentAssertions;
+using NSubstitute;
+using Serilog;
+
+namespace Doktr.Lifters.AsmResolver.Tests.DependencyGraph;
+
+public class AnalyzerFixture<T>
+    where T : IDependencyGraphAnalyzer<IMemberDefinition>
+{
+    public AnalyzerFixture()
+    {
+        Module = ModuleDefinition.FromFile(typeof(AnalyzerFixture<>).Assembly.Location);
+        DependencyGraph = BuildDependencyGraph();
+        Analyzer = (T) Activator.CreateInstance(typeof(T), Logger)!;
+    }
+
+    public ILogger Logger { get; } = Substitute.For<ILogger>();
+    public ModuleDefinition Module { get; }
+    public DependencyGraph<IMemberDefinition> DependencyGraph { get; }
+    public T Analyzer { get; }
+
+    public DependencyNode<IMemberDefinition> GetNodeFor(MemberInfo member)
+    {
+        string needle = GetNeedle();
+        return DependencyGraph.Nodes.Single(m => m.Value.FullName == needle);
+
+        string GetNeedle()
+        {
+            if (member.ReflectedType is null)
+                return ((Type) member).FullName!;
+
+            return $"{member.ReflectedType.FullName}.{member.Name}";
+        }
+    }
+
+    public DependencyNode<IMemberDefinition> AnalyzeNode(MemberInfo member)
+    {
+        var node = GetNodeFor(member);
+        Analyzer.AnalyzeNode(node);
+
+        return node;
+    }
+
+    public void AssertNoWarnings()
+    {
+        int warnings = Logger
+                       .ReceivedCalls()
+                       .Count(c => c.GetMethodInfo().Name != nameof(Logger.Warning));
+
+        warnings.Should().Be(0);
+    }
+
+    private DependencyGraph<IMemberDefinition> BuildDependencyGraph()
+    {
+        var builder = new DependencyGraphBuilder(Array.Empty<IDependencyGraphAnalyzer<IMemberDefinition>>(), Module);
+        return builder.BuildDependencyGraph();
+    }
+
+}
