791b578c0aafc71f0ab30e893062c55429d9cf27
zsr2531 <warhaamer1337@gmail.com>
2022-07-25 10:53:21 +0200

    Make extension method more generic

--- src/Doktr.Core/Models/Collections/CollectionExtensions.cs
+++ src/Doktr.Core/Models/Collections/CollectionExtensions.cs
@@ -2,8 +2,15 @@ namespace Doktr.Core.Models.Collections;
 
 public static class CollectionExtensions
 {
-    public static bool IsEmpty<T>(this ICollection<T> collection)
+    public static bool IsEmpty<T>(this IEnumerable<T> items)
     {
-        return collection.Count == 0;
+        return items switch
+        {
+            IList<T> list => list.Count == 0,
+            IReadOnlyList<T> roList => roList.Count == 0,
+            ICollection<T> coll => coll.Count == 0,
+            IReadOnlyCollection<T> roColl => roColl.Count == 0,
+            _ => !items.Any()
+        };
     }
 }
